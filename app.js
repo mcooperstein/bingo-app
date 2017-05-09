a1e508fd63eb1973c1e8f63a5b99a128266bf528
Marc Cooperstein <mcooperstein@gmail.com>
2017-05-09 10:58:01 -0700

    first functional version

---
+++ app.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+    $("#ball").hide();
+});
+
+var bingo = ['B', 'I', 'N', 'G', 'O'];
+var happyness = [];
+
+function card() {
+    var newLetter = bingo[Math.floor(Math.random() * bingo.length)]
+    var number = Math.floor(Math.random() * 20) + 1;
+    var joined = newLetter + "-" + number;
+
+    if (happyness.includes(joined)) {
+        alert(joined + " has already been called")
+    } else {
+        happyness.push(joined);
+        //outputting to the screen area
+        $("h3").text(joined);
+        $("ul").append("<li>" + joined + ", " + "</li>")
+    }
+
+    console.log(happyness);
+}
+
+$("#flip").click(function () {
+    card();
+    $("#ball").show();
+});
+
+$("#check").click(function () {
+    var ball = $("input").val()
+    if (happyness.includes(ball)) {
+        alert(ball + " has already been called")
+    } else {
+        alert(ball + " was not called")
+    }
+});
